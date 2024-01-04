b5b788793ebb7a82f46c583840bb3c574ade3aa8
LKMN <lucasmenesestroche@gmail.com>
2024-01-04 13:31:06 -0400

    Primer commit

---
+++ src/components/Card/index.jsx
@@ -0,0 +1,19 @@
+import styles from "./Card.module.css"
+
+export default function Card(props) {
+   
+   return (
+      <div className={styles.card}>
+         <button onClick={props.onClose}>X</button>
+         <h1 className={styles.titulo}>{props.name}</h1>
+         <h2>{props.status}</h2>
+         <h2>{props.gender}</h2>
+         <h2>{props.origin}</h2>
+         <h2></h2>
+         <img className={styles.imagen}src={props.image} alt='' />
+
+      </div>
+   );
+   
+}
+
