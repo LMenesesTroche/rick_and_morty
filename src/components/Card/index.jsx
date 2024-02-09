2e0ad4c89bf7ff72cd4d4a640cc07255277fb328
LKMN <lucasmenesestroche@gmail.com>
2024-02-09 10:59:29 -0400

    Estilos

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
-import "./styles.css";
-import {stylesLine} from './stylesLine'
+import styles from './card.module.css'
 import { Link } from "react-router-dom";
 import { addFav, removeFav } from "../../redux/actions"; // Asegúrate de importar las acciones correctas
 import { connect } from "react-redux";
@@ -46,51 +45,40 @@ function Card(props) {
   
 
   return (
-    <div className='container'>
-      {isFav ? (
-        <button onClick={handleFavorite}>❤️</button>
-          ) : (
-          <button onClick={handleFavorite}>🤍</button>
-        )
-      }
+    <div className={styles.container}>
+      <div className={styles.favButton}>
+        {isFav ? (
+          <button onClick={handleFavorite}>❤️</button>
+            ) : (
+            <button onClick={handleFavorite}>🤍</button>
+          )
+        }
+      </div>
+      
       <img
         src={image}
         alt='foto'
         height={200}
         width={200}
-        style={stylesLine.img}
+        className={styles.img}
       />
-      <Link  to={`/detail/${props.id}`}>
-        <h1
-         style={{
-           backgroundColor: "#08B2C9",
-           fontSize: 16,
-          }}
-        >
-          {name}
-        </h1>
-      </Link>
+      <div className={styles.detail}>
+        <Link  to={`/detail/${props.id}`}>
+          <h1 >
+            {name}
+          </h1>
+        </Link>
+      </div>
+     
       
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "column",
-        }}
-      >
+      <div className={styles.data}>
         <h2>{status}</h2>
         <h2>{species}</h2>
         <h2>{gender}</h2>
         <h2>{origin.name}</h2>
       </div>
       
-      <button
-        onClick={() => onClose(props.id)}
-        style={{
-          position: "absolute",
-          top: 8,
-          right: 6,
-        }}
-      >
+      <button onClick={() => onClose(props.id)} className={styles.buttonX}>
         X
       </button>
     </div>
