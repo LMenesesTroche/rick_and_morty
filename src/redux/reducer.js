1766032c2f056bfeaf037b295d69daa485c5638c
LKMN <lucasmenesestroche@gmail.com>
2024-02-13 12:58:10 -0400

    Terminado

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,19 +1,57 @@
-import { ADD_FAV, REMOVE_FAV  } from "./actions";
+import { ADD_FAV, FILTER, ORDER, REMOVE_FAV  } from "./actions";
 
 const intialState = {
     myFavorites:[],
+    allCharacters:[],
 };
 
 
-const rootReducer = (state = intialState, action) =>{
-    switch(action.type){
+const rootReducer = (state = intialState, {type, payload}) =>{
+    switch(type){
         case ADD_FAV:
-            return {...state, myFavorites:[...state.myFavorites, action.payload]}
+            return {
+                ...state,
+                allCharacters:[...state.allCharacters, payload],
+                myFavorites:[...state.myFavorites, payload],
+            };
         case REMOVE_FAV:
-            return {...state, myFavorites:[...state.myFavorites.filter(char=>char.id !== action.payload)]};
+            return {
+                ...state,
+                myFavorites:state.myFavorites.filter((char)=>char.id !== payload),
+            };
+        case FILTER:
+            if(payload === 'All'){
+                return {
+                    ...state,
+                    myFavorites:state.allCharacters,
+                };
+            }
+            return {
+                ...state, 
+                myFavorites:state.allCharacters.filter(
+                    (char)=>char.gender === payload)
+                };
+            
+        case ORDER:
+            if(payload.toUpperCase() === "A"){
+                return{
+                    ...state,
+                    myFavorites: state.allCharacters.sort((a,b)=>
+                    a.name > b.name ? 1 : -1),
+                };
+                
+            }else {
+                return{
+                    ...state,
+                    myFavorites:state.allCharacters.sort((a,b)=>
+                    a.name < b.name ? 1 : -1
+                    ),
+                };
+            }
+            
         default:
 
-            return {...state};
+            return state;
     }
 }; 
 
