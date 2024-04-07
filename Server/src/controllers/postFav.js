211aa33bf491813de3a5b4b88be384664c542865
LKMN <lucasmenesestroche@gmail.com>
2024-04-07 14:07:46 -0400

    Antrs del formateo

--- Server/src/controllers/postFav.js
+++ Server/src/controllers/postFav.js
@@ -1,27 +1,25 @@
-const { Favorite } = require("../models/Favorite")
+const { Favorite } = require("../models/Favorite");
 
 const postFav = async (req, res) => {
-            // res.status(500).json("holis")
+    try {
+        const { id, name, origin, status, image, species, gender } = req.body;
 
-    // try{
-        const { id, name, origin, status, image ,species ,gender } = req.body;
-        //verificamos que esten llegando todos los datos
-        if(!id ||!name || !origin|| !status || !image || !species || !gender ){
-            res.status(401).json("faltan datos")
-            //Si llegan todos los datos agregamos un favorito a mi modelo favorite
-        }
-        try{
-            await Favorite.findOrCreate({
-                where:{ name, origin, status, image ,species ,gender },
-                defaults: { name, origin, status, image ,species ,gender }
-            });
-            const allFavs = await Favorite.findAll();
-            return res.status(200).json({ allFavs })
-
-        }catch(error){
-        res.status(500).json({ error:error.message })
+        // Verificamos que estén llegando todos los datos
+        if (!id || !name || !origin || !status || !image || !species || !gender) {
+            return res.status(401).json("Faltan datos");
         }
 
-}
+        // Si llegan todos los datos, agregamos un favorito a mi modelo favorite
+        await Favorite.findOrCreate({
+            where: { name, origin, status, image, species, gender },
+            defaults: { name, origin, status, image, species, gender }
+        });
 
-module.exports = postFav ;
+        const allFavs = await Favorite.findAll();
+        return res.status(200).json({ allFavs });
+    } catch (error) {
+        return res.status(500).json({ error: error.message });
+    }
+};
+
+module.exports = postFav;
