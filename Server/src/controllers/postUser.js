2e81f3f4f9bedd05bf251cf046079bfff80ecc2f
LMenesesTroche <lucasmenesestroche@gmail.com>
2024-04-08 11:49:52 -0400

    postUser

--- Server/src/controllers/postUser.js
+++ Server/src/controllers/postUser.js
@@ -3,13 +3,14 @@ const { User } = require("../DB_connection");
 const postUser = async (req, res) =>{
         //sacamos el  email y password
         const { email, password } = req.body;
-
+        
         if(!email || !password){
             res.status(400).json("Faltan datos");
         } 
+        // return res.status(500).json({ email, password  });
         try{
             const user = await User.findOrCreate({ where:{ email },defaults:{ password } });
-            return res.json({ user })        
+            return res.json(user)        
         }catch(error){
             return res.status(500).json({ error:error.message  });
         }
